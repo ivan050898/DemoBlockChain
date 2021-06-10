6b90f04985ac4104bcc43bf3a9604f083346c9b8
ivan050898 <44034185+ivan050898@users.noreply.github.com>
2021-06-09 22:31:01 -0600

    Pagos terminados
    se termina la funcionalidad de pagos

--- demo/src/Componentes/Empresa.js
+++ demo/src/Componentes/Empresa.js
@@ -1,18 +1,68 @@
 import React,{useEffect,useState} from 'react'
 import { Fragment } from 'react';
 import '../App.css'
-
+import Web3 from 'web3'
 
 const Empresa = () => {
     const [EthereumPrecio,setEthereumPrecio] = useState('')
     const [Oferta,setOferta] = useState(0)
+    const [web3,setweb3] = useState(0)
+    const [cuenta,setcuenta] = useState(0)
+    const [balance,setbalance] = useState(0)
 
     useEffect(() => {
         fetch('https://api.coincap.io/v2/assets?ids=ethereum')
         .then(response => response.json())
         .then(data => setEthereumPrecio(data.data[0].priceUsd));
-        
+        CargarWeb3();
     }, [])
+    
+    const  CargarWeb3 = async ()=>{
+        if (typeof window.ethereum !== 'undefined') {
+            setweb3( new Web3(window.ethereum));
+
+          try {
+            await window.ethereum.enable();
+          } catch(e) {
+            console.log("gg")
+          }
+        }
+    }
+
+    const CargarCuentas = async ()=>{
+        var accounts = await web3.eth.getAccounts();
+        setcuenta(accounts[0])
+        const balance = await web3.eth.getBalance(accounts[0]);
+        setbalance(balance);
+        const TotalPagar= (((Oferta/EthereumPrecio)*0.05)+(Oferta/EthereumPrecio)).toString()
+        const valorOferta= web3.utils.toWei(TotalPagar)
+        const valorHexadecimal= web3.utils.toHex(valorOferta).toString()   
+        let params= [
+            {
+              from: accounts[0],
+              to: '0xc24101beF39F51DfBC75554f7Ee2F00c11B2a03F',
+              gas: '0x76c0', // 30400
+              gasPrice: '0x9184e72a000', // 10000000000000
+              value: valorHexadecimal, // 2441406250
+              data:
+                '0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675',
+            },
+          ];
+          window.ethereum
+            .request({
+              method: 'eth_sendTransaction',
+              params,
+            })
+            .then((result) => {
+              console.log(result)
+              console.log( web3.eth.getTransaction(result));
+            })
+            .catch((error) => {
+                console.log(error)
+            });
+
+
+    }
 
     return (
         
@@ -27,6 +77,10 @@ const Empresa = () => {
                             <label htmlFor="Descripcion" className="form-label">Descripción</label>
                             <textarea  rows="6" className="form-control" id="Descripcion" />
                         </div>
+                        <div className="mb-3">
+                            <label htmlFor="horas" className="form-label" >Fecha de entrega esperada</label>
+                            <input  className="form-control" id="horas"  type="date" style={{"width":"20%"}} min={new Date().toISOString().split("T")[0]}/>
+                        </div>
                       
                     </form>
                     <div className="row" style={{"paddingTop":"20px"}}>
@@ -36,14 +90,19 @@ const Empresa = () => {
                             </div>
                             <div className="col-sm-3" >
                                 <label  className="form-label">Monto a pagar en Ethereum</label>
-                                <h6>{Oferta/EthereumPrecio}</h6>
+                                <h6>{Oferta/EthereumPrecio} ETH</h6>
+                                <label  className="form-label">Comisión de la plataforma (5%):</label>
+                                <h6> {(Oferta/EthereumPrecio)*0.05} ETH</h6>
+                                <label  className="form-label">Total a pagar:</label>
+                                <h6> {((Oferta/EthereumPrecio)*0.05)+(Oferta/EthereumPrecio)} ETH</h6>
+
                             </div>
                             <div className="col-sm-4" >
                                 <label  className="form-label">Precio de un Ethereum</label>
                                 <h6>1 ETH = ${EthereumPrecio}</h6>
                             </div>
                     </div>
-                    <button type="submit" className="btn btn-dark" style={{"marginTop":"20px"}}>Crear</button>
+                    <button  className="btn btn-dark" style={{"marginTop":"20px"}} onClick={()=>CargarCuentas()}>Crear</button>
 
                 </div>
         </Fragment>
